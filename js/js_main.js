cadbf8179793d09b933677b75484994819ca6013
anonymous-kris <65069057+anonymous-kris@users.noreply.github.com>
2020-07-29 12:40:38 +0100

    progress

---
+++ js/js_main.js
@@ -0,0 +1,56 @@
+var map = L.map("map", {center: [51.1657,8.9515], zoom: 6});
+
+
+
+L.tileLayer(
+    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", 
+    {attribution: "&copy; OpenStreetMap"}
+).addTo(map);
+
+
+var popup = L.popup();
+
+
+function onMapClick(e) {
+	popup
+		.setLatLng(e.latlng)
+		.setContent(
+			"You clickede the map at <br>" + "lon: " +  e.latlng.lng.toFixed(5) + "<br>" + 
+			"Lat: " + e.latlng.lat.toFixed(5))
+		.openOn(map);
+}
+
+
+map.on("click",onMapClick);
+/*
+ Create a marker with latlong object
+L.marker(L.latLng(31.264, 34.802)).addTo(map);
+/*
+
+/*var circle = L.circle(
+  [51.262218, 8.801472 - 0.001], 
+  {radius: 10000, color: "black", fillColor: "red"}
+)
+circle.addTo(map);
+circle.bindPopup("First circle to be ever created!")
+*/
+
+/*var legend = L.control({position: "bottomright"});
+legend.onAdd = function(map) {
+    var div = L.DomUtil.create("div", "legend");
+    div.innerHTML = 
+        '<p><b>Simple shapes in Leaflet</b></p><hr>' +
+        '<p>This map shows an example of adding shapes ' + 
+        'on a Leaflet map</p>' +
+        'The following shapes were added:<br>' +
+        '<p><ul>' +
+        '<li>A marker</li>' +
+        '<li>A line</li>' +
+        '<li>A polygon</li>' +
+        '</ul></p>' +
+        'The line layer has a <b>popup</b>. ' + 
+        'Click on the line to see it!<hr>' +
+        'Created with the Leaflet library<br>'
+    return div;
+};
+legend.addTo(map); */
